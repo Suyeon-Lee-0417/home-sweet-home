845541e4711b0e09005478df62f5384f574fcce9
Carol Ma <carol.ma3728@gmail.com>
2025-03-09 01:12:08 -0800

    added team info when getting a single user

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,5 +1,6 @@
 import {Router, Request, Response} from "express";
 import User, {IUser} from "../model/User";
+import Team from "../model/Team";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,9 +9,15 @@ const router = Router();
 // GET /api/users/:uid
 router.get("/:uid", async (req: Request, res: Response) => {
   try {
-    const user = await User.findOne({firebaseUid: req.params.uid});
+    let user = await User.findOne({firebaseUid: req.params.uid});
     if (!user) return res.status(404).json({message: "User not found"});
-    res.json({user});
+    // findout in which team the user belongs
+    // get the team details
+
+    const team = await Team.findOne({memberIds: user._id});
+    if (!team) return res.json({user});
+
+    res.json({user, team});
   } catch (error) {
     console.error("Error fetching user:", error);
     res.status(500).json({message: "Error fetching user"});
