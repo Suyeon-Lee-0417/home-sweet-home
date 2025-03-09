553cd151cd8f4c44811c68da6d659808860d9163
Carol Ma <carol.ma3728@gmail.com>
2025-03-08 18:46:17 -0800

    updated deploy cmd

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -2,8 +2,7 @@ import admin from "firebase-admin";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_PATH as string);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
